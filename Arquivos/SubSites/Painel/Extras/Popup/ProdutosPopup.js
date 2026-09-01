3ac3d5778fc3c4b106e5369bb502b6cc0f4b7111
Lucas <lucastuleski@outlook.com>
2026-08-31 21:04:38 -0300

    Atualiza sistema de produtos

--- Arquivos/SubSites/Painel/Extras/Popup/ProdutosPopup.js
+++ Arquivos/SubSites/Painel/Extras/Popup/ProdutosPopup.js
@@ -2,7 +2,7 @@ const precosProdutos = [
     "R$ 210,99",
     "R$ 299,99",
     "R$ 320,99",
-    "R$ ,99",
+    "R$ 210,99",
     "R$ ,99",
     "R$ ,99",
     "R$ ,99",
@@ -38,7 +38,7 @@ const produtos = [
     "Floreira de parede <br>vertical jardim com 4 <br>cachepos 1,20x60  sku10",
     "Floreira de parede <br>horizontal jardim 3 <br>prateleiras sku18",
     "Floreira de parede <br>horizontal para jardim <br>externo com 5 cachepots sku21",
-    "",
+    "Floreira de parede <br>vertical jardim 1,20x60 <br>com 4 cachepos sku31",
     "",
     "",
     "",
@@ -121,6 +121,25 @@ Largura: 150 cm <br>
 DIMENSÕES DOS CACHEPOS:<br>
 4 chachepos Largura: 40 x 14 cm<br>
 1 chachepo largura: 50 x 14 cm<br>
+<br>
+FAZEMOS OS PAINÉIS NO TAMANHO DO ESPAÇO DA SUA PAREDE; BASTA NOS PASSAR AS MEDIDAS PARA FAZERMOS O ORÇAMENTO. TRABALHAMOS COM AS TONALIDADES: <br>
+RÚSTICOS FOSCOS E QUEIMADO/SAFARI, IMBUIA, CEREJEIRA, MOGNO, BRANCO, PRETO, CINZA E CAMURÇA |<br>
+<br>
+FAZEMOS OS CACHEPOS EM TAMANHOS MAIORES E FIXOS NO PAINEL - CONSULTE-NOS PELO WHATS<br>
+<br>
+PARA COMPRAS VIA MERCADOLIVRE ACESSE NOSSO LINK E VISITE NOSSA LOJA VIRTUAL.
+<a href="https://lista.mercadolivre.com.br/_CustId_28595610?item_id=MLB3971325067&category_id=MLB271323&seller_id=28595610&client=recoview-selleritems&recos_listing=true#origin=upp&component=sellerData&typeSeller=classic" target="_blank">
+    Acessar site
+</a>
+`,
+`
+Painel estilo ripado nas cores das imagens.
+<br>
+DIMENSÕES PAINEL :<br>
+Altura: 120 cm<br>
+Largura: 60 cm <br>
+DIMENSÕES DOS CACHEPOS:<br>
+4 chachepos Largura: 40 x 14 cm<br>
 <br>
 FAZEMOS OS PAINÉIS NO TAMANHO DO ESPAÇO DA SUA PAREDE; BASTA NOS PASSAR AS MEDIDAS PARA FAZERMOS O ORÇAMENTO. TRABALHAMOS COM AS TONALIDADES: <br>
 RÚSTICOS FOSCOS E QUEIMADO/SAFARI, IMBUIA, CEREJEIRA, MOGNO, BRANCO, PRETO, CINZA E CAMURÇA |<br>
