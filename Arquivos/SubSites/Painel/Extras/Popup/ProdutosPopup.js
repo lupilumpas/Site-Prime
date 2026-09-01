c90092deebd7497dd477e7ffc224fd6b51406cde
Lucas <lucastuleski@outlook.com>
2026-08-31 21:14:26 -0300

    Atualiza sistema de produtos

--- Arquivos/SubSites/Painel/Extras/Popup/ProdutosPopup.js
+++ Arquivos/SubSites/Painel/Extras/Popup/ProdutosPopup.js
@@ -3,7 +3,7 @@ const precosProdutos = [
     "R$ 299,99",
     "R$ 320,99",
     "R$ 210,99",
-    "R$ ,99",
+    "R$ 298,99",
     "R$ ,99",
     "R$ ,99",
     "R$ ,99",
@@ -39,7 +39,7 @@ const produtos = [
     "Floreira de parede <br>horizontal jardim 3 <br>prateleiras sku18",
     "Floreira de parede <br>horizontal para jardim <br>externo com 5 cachepots sku21",
     "Floreira de parede <br>vertical jardim 1,20x60 <br>com 4 cachepos sku31",
-    "",
+    "Floreira de parede <br>vertical jardim <br>com 4 cachepos <br>cor cerejeira 1,20x80  sku82",
     "",
     "",
     "",
@@ -54,22 +54,21 @@ const produtos = [
 ];
 
 const linksComprar = [
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Branco.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Camurça.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Envelhecido.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Cerejeira Claro.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Cerejeira Escuro.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Cinza Claro.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Cinza Escuro.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Imbuia.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Mogno.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Cru/Natural.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Por%20do%20sôl.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Preto.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Caramelo.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Pastel.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Rosê.",
-    "http://api.whatsapp.com/send/?phone=5541991371245&text=Ol%C3%A1%21%20Vim%20pela%20p%C3%A1gina%20JDM%20Madeiras%20e%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20um%20nicho%20Safari."
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
+    "https://api.whatsapp.com/send/?phone=5541991371245&text=Olá! Vim pela página JDM Madeiras e gostaria de solicitar um orçamento para um nicho Safari.",
 ];
 
 const descricaoProduto = [ 
@@ -140,6 +139,25 @@ Altura: 120 cm<br>
 Largura: 60 cm <br>
 DIMENSÕES DOS CACHEPOS:<br>
 4 chachepos Largura: 40 x 14 cm<br>
+<br>
+FAZEMOS OS PAINÉIS NO TAMANHO DO ESPAÇO DA SUA PAREDE; BASTA NOS PASSAR AS MEDIDAS PARA FAZERMOS O ORÇAMENTO. TRABALHAMOS COM AS TONALIDADES: <br>
+RÚSTICOS FOSCOS E QUEIMADO/SAFARI, IMBUIA, CEREJEIRA, MOGNO, BRANCO, PRETO, CINZA E CAMURÇA |<br>
+<br>
+FAZEMOS OS CACHEPOS EM TAMANHOS MAIORES E FIXOS NO PAINEL - CONSULTE-NOS PELO WHATS<br>
+<br>
+PARA COMPRAS VIA MERCADOLIVRE ACESSE NOSSO LINK E VISITE NOSSA LOJA VIRTUAL.
+<a href="https://lista.mercadolivre.com.br/_CustId_28595610?item_id=MLB3971325067&category_id=MLB271323&seller_id=28595610&client=recoview-selleritems&recos_listing=true#origin=upp&component=sellerData&typeSeller=classic" target="_blank">
+    Acessar site
+</a>
+`,
+`
+Painel estilo ripado nas cores das imagens.
+<br>
+DIMENSÕES PAINEL :<br>
+Altura: 120 cm<br>
+Largura: 80 cm <br>
+DIMENSÕES DOS CACHEPOS:<br>
+4 chachepos Largura: 50 x 14 cm<br>
 <br>
 FAZEMOS OS PAINÉIS NO TAMANHO DO ESPAÇO DA SUA PAREDE; BASTA NOS PASSAR AS MEDIDAS PARA FAZERMOS O ORÇAMENTO. TRABALHAMOS COM AS TONALIDADES: <br>
 RÚSTICOS FOSCOS E QUEIMADO/SAFARI, IMBUIA, CEREJEIRA, MOGNO, BRANCO, PRETO, CINZA E CAMURÇA |<br>
